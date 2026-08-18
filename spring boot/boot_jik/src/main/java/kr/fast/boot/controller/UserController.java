cfb778f0103113b05ebe20386ec3645c96cd3cb8
st8324 <stajun@kut.ac.kr>
2026-08-18 17:46:32 +0900

    스프링 02일차 : 데이터 전송 연습

---
+++ spring boot/boot_jik/src/main/java/kr/fast/boot/controller/UserController.java
@@ -0,0 +1,26 @@
+package kr.fast.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.fast.boot.dto.SignupDTO;
+
+@Controller //이 어노테이션이 있어야 컨트롤러가 됨 
+public class UserController {
+
+	
+	@GetMapping("/user/nickname")
+	@ResponseBody //리턴값을 그대로 전달(뷰리졸버 거치지 말고)
+	public String userNickname() {
+		//로그인한 회원 닉네임을 가져왔다 치고 
+		return "홍길동";
+	}
+	
+	@GetMapping("/user/signup")
+	@ResponseBody
+	public String userSignup(SignupDTO dto) {
+		System.out.println(dto);
+		return "회원 가입에 성공했습니다.";
+	}
+}
---
+++ spring boot/boot_jik/src/main/java/kr/fast/boot/dto/SignupDTO.java
@@ -0,0 +1,10 @@
+package kr.fast.boot.dto;
+
+import lombok.Data;
+
+@Data
+public class SignupDTO {
+	private String id;
+	private String pw;
+	private String email;
+}
