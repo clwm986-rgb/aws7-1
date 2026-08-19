3cc3eb1192ba6918811fffb3b093237bbd3decd1
st8324 <stajun@kut.ac.kr>
2026-08-19 12:47:36 +0900

    스프링 03일차 : DB 연동중

---
+++ spring boot/boot_jik/src/main/java/kr/fast/boot/service/UserService.java
@@ -0,0 +1,16 @@
+package kr.fast.boot.service;
+
+import org.springframework.stereotype.Service;
+
+import kr.fast.boot.dto.SignupDTO;
+
+@Service
+public class UserService {
+
+	public void signup(SignupDTO dto) {
+		System.out.println("회원가입 구현 중....");
+		
+	}
+
+	
+}
