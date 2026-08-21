0a0ddc1071a1bc78376c4961f582504a3cea5993
st8324 <stajun@kut.ac.kr>
2026-08-21 11:07:58 +0900

    스프링 05일차 : 게시글 등록 구현 중

--- spring boot/boot_jik/src/main/java/kr/fast/boot/controller/PostController.java
+++ spring boot/boot_jik/src/main/java/kr/fast/boot/controller/PostController.java
@@ -1,8 +1,13 @@
 package kr.fast.boot.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.fast.boot.entity.Board;
 import kr.fast.boot.service.BoardService;
 import lombok.AllArgsConstructor;
 
@@ -13,5 +18,12 @@ public class PostController {
 
 	private final BoardService boardService;
 	
-	
+	@GetMapping("/board")
+	public ResponseEntity<Object> boardGet(){
+		
+		//서비스야. 게시판 목록을 가져와
+		List<Board> list = boardService.getBoardList();
+		
+		return ResponseEntity.ok(list);
+	}
 }
