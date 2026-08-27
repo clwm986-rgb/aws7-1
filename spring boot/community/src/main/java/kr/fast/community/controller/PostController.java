05f6f082800063d40847ce76012c88ea3c6bcae9
st8324 <stajun@kut.ac.kr>
2026-08-27 13:45:01 +0900

    스프링 09일차 : 복습 - 게시글 목록 구현 중

--- spring boot/community/src/main/java/kr/fast/community/controller/PostController.java
+++ spring boot/community/src/main/java/kr/fast/community/controller/PostController.java
@@ -1,8 +1,14 @@
 package kr.fast.community.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.fast.community.entity.Post;
+import kr.fast.community.service.PostService;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -10,4 +16,11 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class PostController {
 	
+	private final PostService postService;
+	
+	@GetMapping("")
+	public ResponseEntity<Object> get(){
+		List<Post> list = postService.getPosts();
+		return ResponseEntity.ok(list);
+	}
 }
--- spring boot/community/src/main/java/kr/fast/community/service/PostService.java
+++ spring boot/community/src/main/java/kr/fast/community/service/PostService.java
@@ -1,11 +1,23 @@
 package kr.fast.community.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import kr.fast.community.entity.Post;
+import kr.fast.community.repository.PostRepository;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class PostService {
 
+	private final PostRepository postRepository;
+
+	public List<Post> getPosts() {
+		//전체 게시글을 가져오는 코드를 작성 해보세요.
+		
+		return null;
+	}
+	
 }
