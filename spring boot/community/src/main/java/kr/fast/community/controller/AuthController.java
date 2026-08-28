55bd04630aa467b66bc3ad6802628e9396888b1c
st8324 <stajun@kut.ac.kr>
2026-08-28 11:23:10 +0900

    스프링 10일차 : 로그인 구현 중

--- spring boot/community/src/main/java/kr/fast/community/controller/AuthController.java
+++ spring boot/community/src/main/java/kr/fast/community/controller/AuthController.java
@@ -1,11 +1,15 @@
 package kr.fast.community.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.fast.community.dto.LoginRequest;
 import kr.fast.community.dto.MessageResponse;
 import kr.fast.community.dto.SignupRequest;
 import kr.fast.community.service.AuthService;
@@ -24,6 +28,21 @@ public class AuthController {
 		try {
 			MessageResponse messageResponse = authService.signup(signupRequest);
 			return ResponseEntity.ok(messageResponse);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.ok(new MessageResponse(false, e.getMessage()));
+		}
+		
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<Object> login(@RequestBody LoginRequest request){
+		Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			//서비스야 사원증 발급해줘. 아이디 비번 줄게
+			String accessToken = authService.login(request);
+			
+			return ResponseEntity.ok("{}");
 		}catch(Exception e) {
 			e.printStackTrace();
 			return ResponseEntity.ok(new MessageResponse(false, e.getMessage()));
